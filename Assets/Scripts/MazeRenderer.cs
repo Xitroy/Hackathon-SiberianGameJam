e9b836ded0bee7980cc1230073d34bcbbde8b375
Daulet Tumbayev <daulettbot@gmail.com>
2021-04-04 17:17:43 +0600

    add map

--- Assets/Scripts/MazeRenderer.cs
+++ Assets/Scripts/MazeRenderer.cs
@@ -41,13 +41,20 @@ public class MazeRenderer : MonoBehaviour
   [Range(1, 50)]
   private int objDensity = 10;
 
+  [SerializeField]
+  private Transform cluePrefab = null;
+
+  [SerializeField]
+  [Range(1, 10)]
+  private int clueDensity = 3;
+
   private int DrawCycle = 0;
 
   // Start is called before the first frame update
   void Start()
   {
     var maze = new CellHive(w, h, uw, uh);
-    DrawWalls(maze, wallPrefab);
+    DrawWalls(maze, wallPrefab, cluePrefab, clueDensity);
     DrawKeys(maze, startPrefab, endPrefab);
     DrawObjects(maze, objPrefabs);
   }
@@ -62,18 +69,24 @@ public class MazeRenderer : MonoBehaviour
       {
         var cell = maze.GetRandomCell();
         var objT = Instantiate(obj, transform) as Transform;
-        var offset = new Vector3((float)rng.NextDouble(), 2 + (float)rng.NextDouble(), 0);
-        var sign = rng.Next(0, 2);
-        if (0 == sign) objT.position = cell.Position + offset;
-        else objT.position = cell.Position - offset;
+        var offset = new Vector3((float)rng.NextDouble(), 1, 0);
+        objT.position = cell.Position + offset;
+        if (objT.position.y <= 0)
+        {
+          objT.position = new Vector3(1, 1, 1);
+        }
         objT.eulerAngles = new Vector3(rng.Next(0, 360), rng.Next(0, 360), rng.Next(0, 360));
       }
     }
   }
 
-  public void DrawWalls(CellHive maze, Transform wallPrefab)
+  public void DrawWalls(CellHive maze, Transform wallPrefab, Transform cluePrefab, int clueDensity)
   {
     DrawCycle++;
+
+    int clueCdMax = (w * h) / clueDensity;
+    int clueCd = 0;
+
     foreach (Cell cell in maze.Cells)
     {
       foreach (Wall wall in cell.Walls.Values)
@@ -81,12 +94,24 @@ public class MazeRenderer : MonoBehaviour
         if (wall.DrawCycle < DrawCycle)
         {
           wall.DrawCycle++;
-          var wallT = Instantiate(wallPrefab, transform) as Transform;
+          Transform wallT;
+
+          if (clueCd <= 0)
+          {
+            clueCd = clueCdMax;
+            wallT = Instantiate(cluePrefab, transform) as Transform;
+          }
+          else
+          {
+            wallT = Instantiate(wallPrefab, transform) as Transform;
+          }
+
           wallT.position = wall.Position;
           wallT.localScale = new Vector3(wall.Scale, wallT.localScale.y, wallT.localScale.z);
           wallT.eulerAngles = wall.Rotation;
         }
       }
+      clueCd--;
     }
   }
 
@@ -95,7 +120,7 @@ public class MazeRenderer : MonoBehaviour
     var start = Instantiate(startPrefab, transform) as Transform;
     start.position = maze.GetStart().Position;
     var end = Instantiate(endPrefab, transform) as Transform;
-    end.position = maze.GetEnd().Position;
+    end.position = maze.GetEnd().Position + new Vector3(0, 2, 0);
   }
 
   // Update is called once per frame
